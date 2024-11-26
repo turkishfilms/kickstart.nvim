8e1bece42241c079ed9d67ab72a9b4ac4d4f7620
Reginald Robinson <ddrbnsn@gmail.com>
2024-11-26 08:40:21 -0800

    removed notify fromnoice

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -27,7 +27,7 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
+ {      'rcarriga/nvim-notify',enabled= false }
     },
     config = function()
       require('noice').setup {
